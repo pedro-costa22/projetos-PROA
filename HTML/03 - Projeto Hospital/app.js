3d3da2ad427aec3567ba66b894ee3c9e53741a8d
Pedro Henrique <phc.contato22@gmail.com>
2022-03-06 12:24:44 -0300

    Projetos HTML - PROA

---
+++ HTML/03 - Projeto Hospital/app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector("#menu-btn");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function(){
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+})
+
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
